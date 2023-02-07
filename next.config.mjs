68d92410a405d904da90e82acb1d7ee86975c4f4
muser <muser@live.dev>
2023-02-07 15:21:25 +0500

    fix next export

--- next.config.mjs
+++ next.config.mjs
@@ -20,10 +20,6 @@ if (isGithubAction) {
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
